b0f4c440709f08393d7f6d91793299b4df2e18bd
UdayGodaba <godabauday@gmail.com>
2023-08-28 10:38:56 +0530

    made all required changes before push

--- index.js
+++ index.js
@@ -25,13 +25,15 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: process.env.DOMAIN_NAME
+}));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 // app.use(helmet({ crossOriginEmbedderPolicy: true }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // File Storage
